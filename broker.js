c8f01ab5c8881680b5d9b7146d123d727d057e29
Menno van Grinsven <scanno71@gmail.com>
2022-05-11 15:28:09 +0200

    mqttbroker: Initial Aedes implementation

--- broker.js
+++ broker.js
@@ -1,5 +1,6 @@
 
-const mosca     = require('mosca');
+var aedes;
+
 var hashpwd = require("./hashpwd.js");
 var globalVar1  = null;
 var logmodule1 = null;
@@ -13,12 +14,15 @@ class brokerMQTT {
 
    constructor(app) {
       this.brokerSettings = {};
+      this.certificate = {};
       this.logmodule = app.logmodule;
       logmodule1 = this.logmodule;
       this.globalVar = app.globalVar;
       globalVar1 = this.globalVar;
       this.server = null;
+      this.serverTLS = null;
       this.serverOnline = false;
+      this.serverRestart = false;
       this.Homey = require('homey');
       globHomey = this.Homey;
       this.OnInit();
@@ -38,6 +42,7 @@ class brokerMQTT {
       var bCertFileValid = false;
 
       this.brokerSettings = {};
+      this.certificate = {};
 
       if (this.Homey.ManagerSettings.get('tls') == true) {
          bTLS = true;
@@ -56,6 +61,7 @@ class brokerMQTT {
             this.logmodule.writelog('debug', "PRIVKEY size = " + privstats.size);
             if (privstats.size > 256){
                 bKeyFileValid = true;
+                this.certificate.key = fs.readFileSync(SECURE_PRIVATEKEY);
             }
          }
          if (fs.existsSync(SECURE_CERT)) {
@@ -64,6 +70,7 @@ class brokerMQTT {
             this.logmodule.writelog('debug', "CERT size = " + certstats.size);
             if (certstats.size > 256) {
                 bCertFileValid = true;
+                this.certificate.cert = fs.readFileSync(SECURE_CERT);
             }
          }
          if (bKeyFileValid === true && bCertFileValid === true) {
@@ -105,12 +112,35 @@ class brokerMQTT {
    }
 
    startBroker() {
+      var ref = this;
       if (this.serverOnline == false && this.getConnectOptions() == true) {
          this.logmodule.writelog('info', "Starting broker...");
          this.logmodule.writelog('info', "brokerSettings: " + JSON.stringify(this.brokerSettings));
          try {
-           this.server = new mosca.Server(this.brokerSettings);
-           this.brokerEvents();
+            var NedbPersistence = require('aedes-persistence-nedb');
+            var db = new NedbPersistence({
+               path: '/userdata',
+               prefix: 'mqtt'
+            });
+            aedes = require('aedes') ();
+            aedes.persistance = db;
+            aedes.authenticate = this.authUser;
+           //aedes.authorizePublish = this.authPublish;
+           //aedes.authorizeSubscribe = this.authSubscribe;
+            this.brokerEvents();
+            this.server = require('net').createServer(aedes.handle);
+            this.serverTLS = require('tls').createServer(this.certificate, aedes.handle);
+
+            this.server.listen(1883, function() {
+               ref.logmodule.writelog('info', 'Aedes MQTT server is up and running');
+               ref.serverOnline = true;
+            });
+
+            this.serverTLS.listen(8883, function() {
+               ref.logmodule.writelog('info', 'Aedes MQTTS server is up and running');
+               ref.serverOnline = true;
+            });
+
          } catch(err) {
            this.logmodule.writelog('error', "startBroker(): " +err);
          }
@@ -124,10 +154,9 @@ class brokerMQTT {
    stopBroker() {
       var ref = this;
       if (this.serverOnline !== false) {
-         this.server.close(function() {
-            ref.logmodule.writelog('info', "Server closed");
-            ref.serverOnline = false;
-         });
+         ref.server.close();
+         ref.serverTLS.close();
+         aedes.close();
       }
    }
 
@@ -135,11 +164,10 @@ class brokerMQTT {
       var ref = this;
       this.logmodule.writelog('info', "Restarting broker...");
       if (this.serverOnline === true) {
-         this.server.close(function() {
-            ref.logmodule.writelog('info', "Broker closed");
-            ref.serverOnline = false;
-            ref.startBroker();
-         });
+         ref.server.close();
+         ref.serverTLS.close();
+         aedes.close();
+         ref.serverRestart = true;
       } else {
          ref.startBroker();
       }
@@ -152,46 +180,43 @@ class brokerMQTT {
    brokerEvents() {
       var ref = this;
       // when a client is connected; the client is passed as a parameter
-      this.server.on('clientConnected', function(client) {
+      aedes.on('client', function(client) {
           ref.logmodule.writelog('info', 'client connected: '+ client.id);
       });
 
-      // when a client is being disconnected; the client is passed as a parameter.
-      this.server.on('clientDisconnecting', function(client) {
-          ref.logmodule.writelog('info', 'client disconnecting: '+ client.id);
-      });
-
       // when a client is disconnected; the client is passed as a parameter.
-      this.server.on('clientDisconnected', function(client) {
+      aedes.on('clientDisconnect', function(client) {
           ref.logmodule.writelog('info', 'client disconnected: '+ client.id);
       });
 
       // when a new message is published; the packet and the client are passed as parameters.
-      this.server.on('published', function(packet, client) {
-        ref.logmodule.writelog('info', 'Packet published '+ packet.payload);
+      aedes.on('publish', function(packet, client) {
+         ref.logmodule.writelog('info', 'Packet published '+ packet.payload);
       });
 
       //  when a client is subscribed to a topic; the topic and the client are passed as parameters.
-      this.server.on('subscribed', function(topic, client) {
-        ref.logmodule.writelog('info', client.id + ' has subscribed to topic '+ topic);
+      aedes.on('subscribe', function(topic, client) {
+         ref.logmodule.writelog('info', client.id + ' has subscribed to topic '+ JSON.stringify(topic));
       });
 
       //  when a client is unsubscribed to a topic; the topic and the client are passed as parameters.
-      this.server.on('unsubscribed', function(topic, client) {
-        ref.logmodule.writelog('info', client.id + ' has unsubscribed from topic '+ topic);
+      aedes.on('unsubscribe', function(topic, client) {
+         ref.logmodule.writelog('info', client.id + ' has unsubscribed from topic '+ JSON.stringify(topic));
+      });
+      aedes.on('closed', function() {
+         ref.logmodule.writelog('info', 'Server is closed');
+         ref.serverOnline = false;
+         if (ref.serverRestart) {
+            ref.serverRestart = false;
+            ref.startBroker();
+         }
       });
 
-      this.server.on('ready', function() {
-         ref.logmodule.writelog('info', 'Mosca server is up and running');
-         ref.server.authenticate = ref.authUser;
-         ref.server.authorizePublish = ref.authPublish;
-         ref.server.authorizeSubscribe = ref.authSubscribe;
-         ref.serverOnline = true;
-      });
    }
 
    // Accepts the connection if the username and password are valid
    authUser(client, username, password, callback) {
+      logmodule1.writelog('info',"User authentication for user " + username);
       if (globHomey.ManagerSettings.get('disable_auth') == false) {
          if (username !== null) {
             var userCheck = globalVar1.getUser(username);
@@ -201,14 +226,15 @@ class brokerMQTT {
                  if (globHomey.ManagerSettings.get('disable_hashing') == false) {
                      if (hashpwd.machPasswords(password, globalVar1.getUser(username).userPassword)) {
                         authorized = true;
-                      }
-                 } else {
-                   logmodule1.writelog('debug', "plain pwd ("+password +" / "+globalVar1.getUser(username).userPassword+")");
-                   if (password ==  globalVar1.getUser(username).userPassword) {
-                     logmodule1.writelog('debug',"plain pwd authorized");
-                     authorized = true;
-                   }
-                 }
+                        logmodule1.writelog('info', 'User '+username+' has been succesfully authenticated');
+                     }
+                  } else {
+                     logmodule1.writelog('debug', "plain pwd ("+password +" / "+globalVar1.getUser(username).userPassword+")");
+                     if (password ==  globalVar1.getUser(username).userPassword) {
+                        logmodule1.writelog('debug',"plain pwd authorized");
+                        authorized = true;
+                     }
+                  }
                }
                if (authorized) client.user = username;
                callback(null, authorized);
@@ -219,38 +245,23 @@ class brokerMQTT {
             callback(null, false);
          }
       } else {
-        callback(null,true);
+         logmodule1.writelog('info', 'User '+username+' is allowed but not checked');
+         callback(null,true);
       }
    }
-
-   /*
-      verifyPassword: Check if the entered password matches the hash
-   */
-/*   verifyPassword(userPassword, hashedPassword) {
-     var password = require('password-hash-and-salt/node_modules/password-hash-and-salt');
-     password(userPassword).verifyAgainst(hashedPassword, function(error, verified) {
-       if (error) {
-         return false;
-       }
-       if (verified) {
-         return true;
-       }
-       return false;
-     });
-   } */
 
    // In this case the client authorized as alice can publish to /users/alice taking
    // the username from the topic and verifing it is the same of the authorized user
    authPublish(client, topic, payload, callback) {
       // right now no check for topic ACL
-      callback(null, true);
+      callback(null);
    }
 
    // In this case the client authorized as alice can subscribe to /users/alice taking
    // the username from the topic and verifing it is the same of the authorized user
    authSubscribe(client, topic, callback) {
       // right now now check for topic ACL
-      callback(null, true);
+      callback(null);
    }
 
    // write and read certificate info
